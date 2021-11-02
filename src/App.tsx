d50a38e5e5d907b44e7ccb560b88ad328ed39f39
Valeron <Lerok.vasil@outlook.com>
2021-11-03 00:38:27 +0500

    add homework-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react"
-import Message from "./components/Message"
 import "./App.css"
+import ChatsPage from "./pages/ChatsPage"
+import { ThemeProvider } from "@mui/material/styles"
+import appTheme from "./constants/appTheme"
 
 const text = "Добрый вечерочек"
 
 function App() {
   return (
     <div className="app">
-      <Message message={text} />
+      <ThemeProvider theme={appTheme}>
+        <ChatsPage></ChatsPage>
+      </ThemeProvider>
     </div>
   )
 }
